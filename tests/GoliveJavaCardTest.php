6fa30f67fbb13bf1eb8a1e694353a7059b92df07
yangjun <yangjun0506@icloud.com>
2021-02-26 11:16:08 +0800

    自动化测试

--- tests/GoliveJavaCardTest.php
+++ tests/GoliveJavaCardTest.php
@@ -20,20 +20,19 @@ use PHPUnit\Framework\TestCase;
 class GoliveJavaCardTest extends TestCase
 {
 
+    /**
+     * java青卡用户信息获取单元测试
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function testGetUserInfo(){
-
         $config  = [
             'appId'=>'H2001081059060727510',
             'appSecret'=>'990617',
             'apiUrl'=>'http://47.114.185.57:8181/account'
         ];
-
         $factory = Factory::goliveJavaCard($config);
-
         $response = $factory->auth->getUserInfo('杨军','18167106183','S10106',144);
-
         var_dump($response);
-
-
+        $this->assertArrayHasKey('code',$response);
     }
 }
